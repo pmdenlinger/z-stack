2d7bde61ba4575e2e4d3a7950505e7b5ee833a01
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 11:31:22 -0700

    Initial Commit

---
+++ z-stack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  z-stack
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ z-stack/z_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  z_stackApp.swift
+//  z-stack
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct z_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
